349d55a5c88567bbfb18b27a5f6854dc5bae7cfe
Samuel <samuel@iMac-de-Andres.local>
2023-09-13 14:41:57 -0500

    task 0 test 1

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: parent node
+ * @value: number contained
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode = NULL;
+
+	newnode = malloc(sizeof(binary_tree_t));
+
+	if (newnode == NULL)
+		return (NULL);
+	
+	newnode->parent = parent;
+	newnode->n = value;
+	newnode->left = NULL;
+	newnode->right = NULL;
+
+
+	return (newnode);
+}
