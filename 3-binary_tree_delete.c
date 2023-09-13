3e449da0f3420da2c95828a7db10c62c01a5c149
Samuel <samuel@iMac-de-Andres.local>
2023-09-13 18:30:43 -0500

    task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: pointer to the root node
+ *
+ * Return: void
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	else
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+	return;
+}
