e0ae111e23bb21f2d821ab3c22a54d208ae8a236
Samuel <samuel@iMac-de-Andres.local>
2023-09-13 19:25:11 -0500

    updated header

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -13,7 +13,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree != NULL && func != NULL)
 	{
 		func(tree->n);
-		binary_tree_preorder(tree->left, &print_num);
-		binary_tree_preorder(tree->right, &print_num);
+		binary_tree_preorder(tree->left, &func);
+		binary_tree_preorder(tree->right, &func);
 	}
 }
